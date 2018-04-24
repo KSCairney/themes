29b5703f608e4896238606633c18bcb765c88789
Kenneth Cairney <kscairney@gmail.com>
2018-04-24 18:41:16 +0000

    working theme buttons

--- js/script.js
+++ js/script.js
@@ -1,29 +1,54 @@
-
+$(document).ready(function(){
+    $('#tut').click(function(){
+        $('#littleman').attr('src', './img/arte2.png');
+        $('#littleplace').attr('src', './img/arte4.png');
+        $('.soil').css('background-color', '#c6b97d');
+        $('.grassland').css('background-color', '#f9e386');
+    });
+    $('#indy').click(function(){
+        $('#littleman').attr('src', './img/arta2.png');
+        $('#littleplace').attr('src', './img/arta4.png');
+        $('.soil').css('background-color', '#876a5b');
+        $('.grassland').css('background-color', '#7fad6b');
+    });
+    $('#chu').click(function(){
+        $('#littleman').attr('src', './img/artc2.png');
+        $('#littleplace').attr('src', './img/artc4.png');
+        $('.soil').css('background-color', '#68614d');
+        $('.grassland').css('background-color', '#fffa9e');
+    });
+    $('#breton').click(function(){
+        $('#littleman').attr('src', './img/artp2.png');
+        $('#littleplace').attr('src', './img/artp4.png');
+        $('.soil').css('background-color', '#685642');
+        $('.grassland').css('background-color', '#3b6d40');
+    });
+});
 
 function holy() {
     document.getElementById("projsect").innerHTML = '<a href="https://damp-retreat-24388.herokuapp.com/"><img src="img/grale.JPG"></a><p class="projname">The Holy GrALE</p><p>A rails website for a micropub with an updatable beer list.</p><i class="devicon-ruby-plain-wordmark"></i><i class="devicon-rails-plain-wordmark"></i><i class="devicon-javascript-plain"></i><i class="devicon-jquery-plain-wordmark"></i><i class="devicon-html5-plain-wordmark"></i><i class="devicon-css3-plain-wordmark"></i><i class="devicon-github-plain-wordmark"></i><i class="devicon-heroku-plain-wordmark"></i>'
-}
+};
 
 function namisgv() {
     document.getElementById("projsect").innerHTML = '<a href="https://limitless-savannah-63020.herokuapp.com/"><img src="img/nsgv.jpg"></a><p class="projname">NAMI San Gabriel Valley</p><p>A rails website for a local charity.</p><i class="devicon-ruby-plain-wordmark"></i><i class="devicon-rails-plain-wordmark"></i><i class="devicon-javascript-plain"></i><i class="devicon-jquery-plain-wordmark"></i><i class="devicon-html5-plain-wordmark"></i><i class="devicon-css3-plain-wordmark"></i><i class="devicon-git-plain-wordmark"></i><i class="devicon-heroku-plain-wordmark"></i>'
-}
+};
 
 function bikeb() {
     document.getElementById("projsect").innerHTML = '<a href="https://kscbikeberlin.herokuapp.com/"><img src="img/bikeberlin.jpg"></a><p class="projname">Bike Berlin</p><p>A used bike eCommerce site.</p><i class="devicon-ruby-plain-wordmark"></i><i class="devicon-rails-plain-wordmark"></i><i class="devicon-javascript-plain"></i><i class="devicon-jquery-plain-wordmark"></i><i class="devicon-html5-plain-wordmark"></i><i class="devicon-css3-plain-wordmark"></i><i class="devicon-github-plain-wordmark"></i><i class="devicon-heroku-plain-wordmark"></i>'
-}
+};
 
 function port2() {
     document.getElementById("projsect").innerHTML = '<a href="https://kscairney.github.io/K-S-Cairney/"><img src="img/porto.JPG"></a><p class="projname">Alt Portfolio</p><p>A simpler alternative portfolio.</p><i class="devicon-javascript-plain"></i><i class="devicon-jquery-plain-wordmark"></i><i class="devicon-html5-plain-wordmark"></i><i class="devicon-css3-plain-wordmark"></i><i class="devicon-github-plain-wordmark"></i>'
-}
+};
 
 function tsun() {
      document.getElementById("projsect").innerHTML = '<a href="https://kscairney.github.io/Tsundoku/"><img src="img/tsundoku.jpg"></a><p class="projname">Tsundoku</p><p>A book blog with random book recommendations.</p><i class="devicon-javascript-plain"></i><i class="devicon-jquery-plain-wordmark"></i><i class="devicon-html5-plain-wordmark"></i><i class="devicon-css3-plain-wordmark"></i><i class="devicon-github-plain-wordmark"></i>'
-}
+};
 
 function hop() {
     document.getElementById("projsect").innerHTML = '<img src="img/hopping.jpg"><p class="projname">Hopping the Pond</p><p>A travel blog with polaroid posts.</p><i class="devicon-javascript-plain"></i><i class="devicon-jquery-plain-wordmark"></i><i class="devicon-html5-plain-wordmark"></i><i class="devicon-css3-plain-wordmark"></i><i class="devicon-github-plain-wordmark"></i>'
-}
+};
 
 function paint() {
     document.getElementById("projsect").innerHTML = '<a href="https://kscairney.github.io/PaintyMcPixel/"><img src="img/pain.jpg"></a><p class="projname">Painty Pixel</p><p>A simple javascript drawing app.</p><i class="devicon-javascript-plain"></i><i class="devicon-jquery-plain-wordmark"></i><i class="devicon-html5-plain-wordmark"></i><i class="devicon-css3-plain-wordmark"></i><i class="devicon-github-plain-wordmark"></i>'
-}
+};
